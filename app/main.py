3e9cd897cf688373b3c0a3bf10bfcd4009b53385
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2020-08-04 10:40:07 -0500

    Added echo logging.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
-from flask import Flask 
+from flask import Flask, request
+import logging
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_view(): 
     return "<h1>Hello World</h1>"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    logging.info("Received the following request: {}".format(request.get_json()))
+    return request.get_json()

78c464cfa34022704849da51f292258c59b24f45
Phi Thuan Au <apt.joanna@gmail.com>
2020-08-05 16:45:23 -0500

    Added error handling.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import logging
+import sys
 logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
@@ -11,4 +12,10 @@ def home_view():
 @app.route("/callback", methods=['POST'])
 def callback():
     logging.info("Received the following request: {}".format(request.get_json()))
+    try:
+        return request.get_json()
+    except Exception:
+        logging.error("Unable to process the request. Received to {}".format(sys.exc_info()[0]))
+        return "Unable to process the request."
+
     return request.get_json()
